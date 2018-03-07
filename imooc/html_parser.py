fd8c4ea2cf57c53b35318e49056d57c8802e10eb
wu0792 <wu0792@163.com>
2018-03-07 08:19:13 +0800

    增加支持依次爬取多个关键字

--- imooc/html_parser.py
+++ imooc/html_parser.py
@@ -6,11 +6,16 @@ from urllib.parse import urljoin
 class HtmlParser(object):
     def _get_new_urls(self, url, soup):
         url_list = set()
-        #/item/%E8%87%AA%E7%94%B1%E8%BD%AF%E4%BB%B6
-        links = soup.find_all('a', href=re.compile(r'^/item/.*'))
-        for link in links:
-            new_url = urljoin(url, link['href'])
+        #<div class="page"><a>首页</a><a>上一页</a><a>1</a>
+        #<a href="javascript:void(0)" class="active text-page-tag">2</a><span class="disabled_page">下一页</span><span class="disabled_page">尾页</span></div>
+        active_link = soup.find('a', class_='active text-page-tag')
+        next_link = active_link.next_sibling
+
+        #下一页有效时候，追加
+        if(''.join(next_link.get('class')) == 'text-page-tag'):
+            new_url = urljoin(url, next_link.get('href'))
             url_list.add(new_url)
+
         return url_list
 
     def _get_new_data(self, url, soup):
@@ -22,10 +27,11 @@ class HtmlParser(object):
 
             node_title = item.find('a')
             new_data['title'] = node_title.get_text().strip()
-            new_data['url'] = node_title.get('href')
+            new_data['url'] = urljoin(url, node_title.get('href'))
 
             #<div class="course-item-classify"><span>实战</span><span>张三讲师</span><span>高级</span><span>4567</span>
-            detail_types = item.find('div', class_='course-item-classify').find_all('span')
+            detail_types = item.find(
+                'div', class_='course-item-classify').find_all('span')
 
             the_type = detail_types[0].get_text()
             the_grade = detail_types[2].get_text()
@@ -36,7 +42,7 @@ class HtmlParser(object):
             new_data['play_count'] = the_play_count.strip()
 
             new_data_list.append(new_data)
-            
+
         return new_data_list
 
     def parse(self, url, content):
--- imooc/main.py
+++ imooc/main.py
@@ -3,6 +3,7 @@ import html_downloader
 import html_outputer
 import html_parser
 import url_manager
+from urllib.parse import urljoin, urlencode, quote_plus
 
 
 class SpiderMain(object):
@@ -12,15 +13,18 @@ class SpiderMain(object):
         self.parser = html_parser.HtmlParser()
         self.outputer = html_outputer.HtmlOutputer()
 
-    def start(self, root_url):
-        self.urls.add_url(root_url)
+    def getRootUrl(self, baseUrl, search):
+        return urljoin(baseUrl, '?words=' + quote_plus(search))
+
+    def start(self):
         while self.urls.has_new_url():
             new_url = self.urls.get_new_url()
             try:
+                print('catch url: ', new_url)
                 html_cont = self.downloader.download(new_url)
 
                 new_urls, new_data_list = self.parser.parse(new_url, html_cont)
-                # self.urls.add_urls(new_urls)
+                self.urls.add_urls(new_urls)
                 self.outputer.collect_data(new_data_list)
             except:
                 print('fail:' + new_url)
@@ -28,9 +32,17 @@ class SpiderMain(object):
         self.outputer.ouput_html()
         print('finish.')
 
+
 print(__name__)
 if __name__ == '__main__':
     print('start.')
-    root_url = 'https://www.imooc.com/search/course?words=python'
+    base_url = 'https://www.imooc.com/search/course'
     spider = SpiderMain()
-    spider.start(root_url)
+    all_words = ['python', 'javascript', '前端',
+                 'reactjs', '区块链', 'java', 'redis']
+
+    for word in all_words:
+        url = spider.getRootUrl(base_url, word)
+        spider.urls.add_url(url)
+
+    spider.start()
